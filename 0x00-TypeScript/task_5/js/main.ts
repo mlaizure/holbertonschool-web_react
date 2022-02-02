12f4aa5e3983c6832f9e90d6f3ab07f36ed80dca
mlaizure <maddi.laizure@gmail.com>
2022-02-02 05:32:30 +0000

    Task 11

---
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -0,0 +1,17 @@
+export interface MajorCredits {
+    credits: number;
+    brand: 'majorCredits';
+}
+
+export interface MinorCredits {
+    credits: number;
+    brand: 'minorCredits';
+}
+
+export const sumMajorCredits = (subject1: MajorCredits, subject2: MajorCredits) => {
+    return { credits: subject1.credits + subject2.credits } as MajorCredits;
+}
+
+export const sumMinorCredits = (subject1: MinorCredits, subject2: MinorCredits) => {
+    return { credits: subject1.credits + subject2.credits } as MinorCredits;
+}
