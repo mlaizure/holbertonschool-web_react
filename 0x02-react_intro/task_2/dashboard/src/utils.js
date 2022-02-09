71ff1c48e0f7596477e00c9de742a139200a6868
mlaizure <maddi.laizure@gmail.com>
2022-02-08 18:38:41 -0600

    Adding some notifications

--- 0x02-react_intro/task_2/dashboard/src/utils.js
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -4,3 +4,7 @@ export const getFooterCopy = (isIndex) => {
   if (isIndex) return 'Holberton School';
   else return 'Holberton School main dashboard';
 }
+
+export const getLatestNotification = () => {
+  return '<strong>Urgent requirement</strong> - complete by EOD'
+}
