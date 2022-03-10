ae1a0f163db6f9ac24b4a560264d6d9a94d8328d
mlaizure <maddi.laizure@gmail.com>
2022-03-10 16:38:11 -0600

    Copying over files from task 2

---
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,44 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import NotificationItem from './NotificationItem';
+import Notifications from './Notifications';
+import { shallow, mount } from 'enzyme';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+StyleSheetTestUtils.suppressStyleInjection();
+
+describe('component rendering for NotificationItem', () => {
+  it('renders item without crashing', () => {
+    shallow(<NotificationItem />);
+  });
+
+  it('renders correct item with type and value props', () => {
+    const wrapper = shallow(<NotificationItem type='default' value='test' />);
+    expect(wrapper.text()).toEqual('test');
+    expect(wrapper.props()['data-notification-type']).toEqual('default');
+  });
+
+  it('renders correct item with type and html props', () => {
+    const wrapper = shallow(<NotificationItem type='default' html={{ __html: '<u>test</u>' }} />)
+    expect(wrapper.html()).toContain('<u>test</u>');
+    expect(wrapper.props()['data-notification-type']).toEqual('default');
+  });
+});
+
+describe('clicking on notification', () => {
+  it('verifies correct id is sent', () => {
+    const listNotifications = [
+      {id: 1, type: 'default', value: 'New course available'},
+      {id: 2, type: 'urgent', value: 'New resume available'}
+    ];
+
+    const mockMarkNotificationAsRead = jest.fn();
+    const wrapper = mount(<Notifications displayDrawer={true} listNotifications={listNotifications} markNotificationAsRead={mockMarkNotificationAsRead}/>);
+    wrapper.find('li').first().simulate('click');
+    expect(mockMarkNotificationAsRead).toBeCalledWith(1);
+    wrapper.find('li').last().simulate('click');
+    expect(mockMarkNotificationAsRead).toBeCalledWith(2);
+  });
+});
