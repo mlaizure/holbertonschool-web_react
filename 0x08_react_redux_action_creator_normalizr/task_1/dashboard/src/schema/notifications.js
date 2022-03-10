a92c3a9f3d06237234d151ca887349831984b33f
mlaizure <maddi.laizure@gmail.com>
2022-03-10 16:06:50 -0600

    Installed normalizr and used it to create schema for notificaitons and added tests for schema

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,7 +1,20 @@
 import notifications from '../../../../notifications.json';
+import { normalize, schema } from 'normalizr';
 
-export default function getAllNotificationsByUser(userId) {
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, {
+  idAttribute: 'guid'
+});
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+const normalizedNotifications = normalize(notifications, [notification]);
+
+function getAllNotificationsByUser(userId) {
   return notifications
     .filter((notification) => notification.author.id === userId)
     .map((notification) => notification.context);
 }
+
+export { normalizedNotifications, getAllNotificationsByUser };
